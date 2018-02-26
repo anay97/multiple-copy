4f576995430907e48ab66ba4b0848a5c96c4e560
ishita1297 <ishita1297@yopmail.com>
2018-02-26 03:08:51 -0500

    Added Content

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
+
+<form method="post" action="index.php">
+  
+<select name="site"  onchange="this.form.submit()">
+  <option name="site0" >Select Site</option>
+  <option value="site1">Site 1</option>
+  <option value="site2">Site 2</option>
+
+</select>
+</form>
+
 <?php
-$a='hi';
+//error_reporting(0);
+session_start();
+$site='';
+if(!isset($_SESSION['site'])){
+  $_SESSION['site']='site1';
+  $site='site1';
+}
+$site=$_POST['site'];
+if($site!='' || $site!=null){
+  $_SESSION['site']=$site;
+}
+echo $site;
+
 ?>
-
-<b>This is working <?php echo $a; ?></b>
-
